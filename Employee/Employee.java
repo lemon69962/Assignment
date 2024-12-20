1ace53895a4d8620f6b827bafd19203e28354d7f
KyleDolce <tan_kai_jie@soc.uum.edu.my>
2024-12-20 10:35:44 +0800

    update getter setter

--- Employee/Employee.java
+++ Employee/Employee.java
@@ -1,1 +1,41 @@
 package Employee;
+
+public class Employee{
+    private int ID;
+    private String name;
+    private double salary;
+
+    public Employee(int ID, String name, double salary){
+        this.ID = ID;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getID(){
+        return ID;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public void setID(int ID){
+        this.ID = ID;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+
+    public String toString(){
+        return "Employee: " + getID() + ", Name: " + getName() + ", Salary: " + getSalary();
+    }
+}
