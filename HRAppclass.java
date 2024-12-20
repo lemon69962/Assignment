e02876f1d747cf0fa8cc3b909b4a1e8d9c30a7f0
KyleDolce <tan_kai_jie@soc.uum.edu.my>
2024-12-20 12:13:39 +0800

    add employee for testing

--- HRAppclass.java
+++ HRAppclass.java
@@ -4,16 +4,18 @@ public class HRAppclass {
 
     public static void main(String[] args) {
         System.out.println("HRApp Starts: ");
-        Employee s1 = new Employee(69, "Voon Xien", 56000);
         Employee s2 = new Employee(70, "Kai Jie", 40000);
+        Employee s3 = new Employee(71, "Ming Sheng", 40000);
+        
 
         Department d1 = new Department("BUSSINESS");
-        d1.addEmployee(s1);
+        d1.addEmployee(new Employee(69, "Voon Xien", 56000));
         d1.addEmployee(s2);
+        d1.addEmployee(s3);
 
         Employee[] employees = d1.getEmployees();
         for (Employee employee : employees) {
-            if (employee != null) {  // Only print if employee is not null
+            if (employee != null) {
                 System.out.println("Employee " + employee);
             }
         }
