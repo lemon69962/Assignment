f23c7153c45c2ac033496a6ed287b0a75d290c09
KyleDolce <tan_kai_jie@soc.uum.edu.my>
2024-12-20 11:16:53 +0800

    update on department

--- Department/Department.java
+++ Department/Department.java
@@ -3,5 +3,26 @@ import Employee.Employee;
 public class Department {
     private String name;
 
-    private Employee[] employees = new Employee[11];
+    private Employee[] employees = new Employee[5];
+
+    private int lastAdditionalEmployeeIndex = -1;
+
+    public Department(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void addEmployee(Employee plusEmployee){
+        if(lastAdditionalEmployeeIndex < employees.length){
+            lastAdditionalEmployeeIndex++;
+            employees[lastAdditionalEmployeeIndex] = plusEmployee;
+        }
+    }
 }
