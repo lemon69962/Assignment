0a93ed4962906cf2ad179a9b963b525fae4b1e15
KyleDolce <tan_kai_jie@soc.uum.edu.my>
2024-12-20 12:04:53 +0800

    Update HRAppclass.java

--- HRAppclass.java
+++ HRAppclass.java
@@ -1,15 +1,22 @@
 
-import Employee.Employee;
 import Department.Department;
+import Employee.Employee;
 public class HRAppclass {
 
     public static void main(String[] args) {
+        System.out.println("HRApp Starts: ");
         Employee s1 = new Employee(69, "Voon Xien", 56000);
         Employee s2 = new Employee(70, "Kai Jie", 40000);
 
-        System.out.println(s1);
-        System.out.println(s2);
 
+        Department d1 = new Department("BUSSINESS");
+        d1.addEmployee(s1);
+        d1.addEmployee(s2);
+
+        Employee[] employees = d1.getEmployees();
+        for (Employee employee : employees) {
+            System.out.println("Employee "+employee);
+        }
     }
 
 }
